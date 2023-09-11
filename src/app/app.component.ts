bedac625007cc381b58c05461a768d47b48087c6
Andreas Krummsdorf <andreas.krummsdorf@doksafe.de>
2023-09-11 23:05:57 +0200

    Bootstrap the application using standalone APIs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component, ViewContainerRef } from '@angular/core';
 import { HostElementService } from './shared/modal/host/host-element.service';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    standalone: true,
+    imports: [
+        RouterLink,
+        RouterLinkActive,
+        RouterOutlet,
+    ],
 })
 export class AppComponent {
   constructor(
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,18 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { AppRoutingModule } from './app/app-routing.module';
+import { CustomersModule } from './app/customers/customers.module';
+import { FormsModule } from '@angular/forms';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, FormsModule, CustomersModule, AppRoutingModule),
+        provideAnimations()
+    ]
+})
   .catch((err) => console.error(err));
